5e2e94933df6a45b205cddf9e274d219e9e18ebf
Satyam Singh <97077594+Satyxm@users.noreply.github.com>
2024-07-24 12:04:30 +0530

    Printing Inverted Triangle Pattern

---
+++ InvertedTrianglePattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class InvertedTrianglePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printInvertedTrianglePattern(n);
+        sc.close();
+    }
+
+    public static void printInvertedTrianglePattern(int size) {
+        int i, j;
+
+        for (i = size; i >= 1; i--) {
+            for (j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
