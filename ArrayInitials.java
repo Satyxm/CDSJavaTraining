575cb97833a433dbf79e888dc7c12b3682b8fba7
Satyam Singh <97077594+Satyxm@users.noreply.github.com>
2024-07-24 15:56:54 +0530

    Create ArrayInitials.java

---
+++ ArrayInitials.java
@@ -0,0 +1,13 @@
+public class ArraysInitials {
+    public static void main(String[] args) {
+        String[] friendsNames = {"Kendrick", "Tupac", "JayZ", "Biggie", "Eminem"};
+        System.out.println(friendsNames[2]);
+        System.out.println(friendsNames.length);
+
+        for (int i = 0; i < friendsNames.length; i++)
+
+        {
+            System.out.println(friendsNames[i]);
+        }
+    }
+}
