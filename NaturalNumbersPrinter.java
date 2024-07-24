7c475e75f6de2d5178ff666257af4317aab11278
Satyam Singh <97077594+Satyxm@users.noreply.github.com>
2024-07-24 11:43:04 +0530

    Loops for printing Natural Numbers

---
+++ NaturalNumbersPrinter.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class NaturalNumbersPrinter {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        int num = 1;
+        while (num <= 10) {
+            System.out.print(num + " ");
+            num++;
+        }
+        System.out.println();
+
+        num = 10;
+        while (num >= 1) {
+            System.out.print(num + " ");
+            num--;
+        }
+        System.out.println();
+
+        num = 1;
+        do {
+            System.out.print(num + " ");
+            num++;
+        } while (num <= 10);
+        System.out.println();
+
+        num = 10;
+        do {
+            System.out.print(num + " ");
+            num--;
+        } while (num >= 1);
+        System.out.println();
+    }
+}
