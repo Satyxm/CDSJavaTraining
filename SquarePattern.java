bd5f8b9660422486a40f04b7a43439fc095e254f
Satyam Singh <97077594+Satyxm@users.noreply.github.com>
2024-07-24 12:02:10 +0530

    Printing Square Pattern

---
+++ SquarePattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SquarePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printSquarePattern(n);
+        sc.close();
+    }
+
+    public static void printSquarePattern(int size) {
+        int i, j;
+
+        for (i = 1; i <= size; i++) {
+            for (j = 1; j <= size; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
