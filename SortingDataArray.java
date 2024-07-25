0aa8c846ecbdb27c3e7b2c2f8b87742c3678cb2e
Satyam Singh <97077594+Satyxm@users.noreply.github.com>
2024-07-25 09:14:08 +0530

    Sorting Array Data

---
+++ SortingDataArray.java
@@ -0,0 +1,23 @@
+public class SortingDataArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 24, 9, 11, 7, 3};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.print("Sorted Array: [");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
+}
