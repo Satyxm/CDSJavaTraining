694c2825d6bd08d0d1019f4108a6d269f32ec58d
Satyam Singh <97077594+Satyxm@users.noreply.github.com>
2024-07-26 10:04:06 +0530

    BMI

--- BMICalc.java
+++ BMICalc.java
@@ -5,11 +5,18 @@ public class BMICalc {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the weight in kgs: ");
         float weight = sc.nextFloat();
-        System.out.println("Enter the height in cm: ");
+        System.out.println("Enter the height in metre: ");
         float height = sc.nextFloat();
-        height = height/100;
         float result = weight/(height*height) ;
 
-        System.out.println("BMI is: " + result);
+        System.out.println("BMI is: " + result + " kg/m^2");
+
+        if(result <= 18.5){
+            System.out.println("You are Underweight!");
+        } else if(result >= 18.5 && result <= 25) {
+            System.out.println("You are Normal!");
+        } else if (result > 25) {
+            System.out.println("You are Overweight!");
+        }
     }
 }
