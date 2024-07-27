b1ebeed3af2a32c764eb752a3510925418742b42
satyxm <singhsatyam4702@gmail.com>
2024-07-27 21:31:15 +0530

    Reverse String

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Billy Butcher";
+        String reversedStr = reverseString(str);
+        System.out.println("Reversed String: " + reversedStr);
+    }
+
+    public static String reverseString(String str) {
+        String rev = " ";
+        for (int i = str.length() - 1; i >= 0; i--){
+           rev += str.charAt(i);
+
+        }
+        return rev.trim();
+
+    }
+}
