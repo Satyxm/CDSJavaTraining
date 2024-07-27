55b5390bb23bc8aaabbbb4435509b12977cfe1e6
satyxm <singhsatyam4702@gmail.com>
2024-07-27 15:32:22 +0530

    GUI Added

--- OTPGeneratorGUI.java
+++ OTPGeneratorGUI.java
@@ -1,2 +1,31 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 public class OTPGeneratorGUI {
+    public static void main(String[] args) {
+        System.out.println("This is an OTP Generator GUI Application");
+        JFrame jFrame = new JFrame(("OTP Generator"));
+
+        JButton otpButton = new JButton("Click to Generate OTP");
+        otpButton.setBounds(200, 200, 200, 50);
+
+        jFrame.add(otpButton);
+
+        otpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Random random = new Random();
+
+                int myOTP = random.nextInt(100000);
+                otpButton.setText(String.valueOf(myOTP));
+            }
+        });
+
+        jFrame.setLayout(null);
+        jFrame.setSize(600, 600);
+        jFrame.setVisible(true);
+
+    }
 }
--- SimpleCalc.java
+++ SimpleCalc.java
@@ -1,2 +1,77 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class SimpleCalc {
+    public static void main(String[] args) {
+
+        //GUI Window
+        JFrame jFrame = new JFrame("Simple Calculator");
+        jFrame.setSize(600, 600);
+        jFrame.setLayout(null);
+
+        //setting visibility of JFrame to true
+        jFrame.setVisible(true);
+
+        //Display Bar
+        JTextField display = new JTextField();
+        display.setEditable(false);
+        display.setBounds(50, 25, 300, 50);
+        jFrame.add(display);
+
+        //Num Buttons
+        JButton[] numButtons = new JButton[10];
+        for (int i = 0; i < 10; i++) {
+            numButtons[i] = new JButton(String.valueOf(i));
+            numButtons[i].setFocusable(false);
+            int finalI = i;
+            numButtons[i].addActionListener(e -> display.setText(display.getText().concat(String.valueOf(finalI))));
+        }
+
+        //Operations Buttons
+        JButton sumButton = new JButton("+");
+        JButton subButton = new JButton("-");
+        JButton mulButton = new JButton("*");
+        JButton divButton = new JButton("/");
+        JButton decButton = new JButton(".");
+        JButton equButton = new JButton("=");
+        JButton delButton = new JButton("del");
+        JButton clrButton = new JButton("clr");
+
+        //GUI Panel
+        JPanel panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+
+        //Adding Buttons to Panel
+        panel.add(numButtons[1]);
+        panel.add(numButtons[2]);
+        panel.add(numButtons[3]);
+        panel.add(sumButton);
+        panel.add(numButtons[4]);
+        panel.add(numButtons[5]);
+        panel.add(numButtons[6]);
+        panel.add(subButton);
+        panel.add(numButtons[7]);
+        panel.add(numButtons[8]);
+        panel.add(numButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+
+
+        //Adding Panel to JFrame
+        jFrame.add(panel);
+
+        //Setting Bounds del, clr
+        delButton.setBounds(50, 400, 100, 50);
+        clrButton.setBounds(250, 400, 100, 50);
+
+        //Adding del, clr to Frame
+        jFrame.add(delButton);
+        jFrame.add(clrButton);
+
+
+    }
 }
